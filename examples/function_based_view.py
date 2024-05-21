5a9527a9071b789f3c503cc5b5740fc5f3918a3a
Makai <tronghaibk2008@gmail.com>
2024-05-21 15:12:08 +0700

    Add an example for function-based view

---
+++ examples/function_based_view.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+from audit_logger import FlaskAuditLogger
+
+app = Flask(__name__)
+audit = FlaskAuditLogger(app)
+
+
+@app.route('/api/v1/users', methods=['GET'])
+@audit.log(action_id='LIST_USERS', description='Fetch a list of users')
+def list_users():
+    resp = jsonify({
+        'users': [
+            {
+                'id': 1,
+                'name': 'Makai'
+            },
+            {
+                'id': 2,
+                'name': 'TNiaH'
+            }
+        ]
+    })
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/api/v1/users', methods=['POST'])
+@audit.log(action_id='CREATE_USER', description='Create a new user')
+def create_user():
+    if request.is_json:
+        name = request.json.get('name')
+        if not name:
+            resp = jsonify({
+                'error': 'validation_failed',
+                'error_description': 'Please provide a name.'
+            })
+            resp.status_code = 400
+            return resp
+
+        return jsonify({
+            'id': 1,
+            'name': name
+        })
+
+    resp = jsonify({
+        'error': 'unsupported_media_type',
+        'error_description': 'Media type is not supported.'
+    })
+    resp.status_code = 415
+    return resp
+
+
+@app.route('/api/v1/users/<int:user_id>', methods=['GET'])
+@audit.log(action_id='GET_USER', description='Fetch a single user by ID')
+def get_user(user_id: int):
+    resp = jsonify({
+        'id': user_id,
+        'name': 'Makai'
+    })
+    resp.status_code = 200
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
